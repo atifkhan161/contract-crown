80df0bdf2a84595d65089883055958ff6a724b4c
Atif khan <atifkhan161@gmail.com>
2025-08-13 16:13:26 +0530

    Update Styles to use theme variables

--- client/src/core/ThemeSwitcher.js
+++ client/src/core/ThemeSwitcher.js
@@ -1,7 +1,7 @@
 // Theme Switcher - Contract Crown PWA
 class ThemeSwitcher {
   constructor() {
-    this.currentTheme = localStorage.getItem('theme') || 'forest';
+    this.currentTheme = localStorage.getItem('theme') || 'deepSea';
     this.applyTheme(this.currentTheme);
   }
 
@@ -16,7 +16,7 @@ class ThemeSwitcher {
   }
 
   getAvailableThemes() {
-    return ['earth', 'blue', 'forest', 'pastel', 'deepSea'];
+    return ['deepSea', 'earth', 'blue', 'forest', 'pastel', ];
   }
 
   getCurrentTheme() {
